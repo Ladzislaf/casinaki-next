25888d641d833c908dbae57ecb90badb447742d9
ladzislaf <ladzislaf@gmail.com>
2023-03-11 23:09:17 +0300

    changes & fixes

--- client/src/pages/Auth/Auth.jsx
+++ client/src/pages/Auth/Auth.jsx
@@ -1,10 +1,10 @@
 import { observer } from 'mobx-react-lite'
 import React, { useContext, useState } from 'react'
 import { NavLink, useLocation, useNavigate } from 'react-router-dom'
-import { Context } from '..'
-import { login, registration } from '../http/userAPI'
-import styles from '../style/Auth.module.css'
-import { LOGIN_ROUTE, MAIN_ROUTE, REGISTER_ROUTE } from '../utils/constants'
+import { Context } from '../..'
+import { login, registration } from '../../http/userAPI'
+import styles from './Auth.module.css'
+import { LOGIN_ROUTE, MAIN_ROUTE, REGISTER_ROUTE } from '../../utils/constants'
 
 const Auth = observer(() => {
 	const { user } = useContext(Context)
@@ -14,6 +14,7 @@ const Auth = observer(() => {
 	const [email, setEmail] = useState('')
 	const [username, setUsername] = useState('')
 	const [password, setPassword] = useState('')
+	const [passwordConfirm, setPasswordConfirm] = useState('')
 
 	const submitHandler = async (e) => {
 		e.preventDefault()
@@ -22,6 +23,8 @@ const Auth = observer(() => {
 			if (isLogin) {
 				data = await login(email, password)
 			} else {
+				if(!validateInputs())
+					return
 				data = await registration(email, username, password)
 			}
 			user.setUser(data)
@@ -32,14 +35,34 @@ const Auth = observer(() => {
 		}
 	}
 
+	const validateInputs = () => {
+		if (!/^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/.test(email)) {
+			alert('Incorrect email format')
+			return false
+		}
+		if (username.length < 5 || username.length > 20) {
+			alert('Username length must be from 5 to 20')
+			return false
+		}
+		if (password.length < 8) {
+			alert('Password must be 8 or more characters')
+			return false
+		}
+		if (password !== passwordConfirm) {
+			alert('Passwords dont match')
+			return false
+		}
+		return true
+	}
+
 	return (
 		<div className={styles.container} style={{ height: window.innerHeight - 54 }}>
 			{isLogin ? <h2>authorization</h2> : <h2>registration</h2>}
 			<form>
-				<input type='email' placeholder='Input email' value={email} onChange={e => setEmail(e.target.value)} required />
+				<input type='email' placeholder='INPUT EMAIL' value={email} onChange={e => setEmail(e.target.value)} required />
 				{isLogin ?
 					<>
-						<input type='password' placeholder='Input password' value={password} onChange={e => setPassword(e.target.value)} required />
+						<input type='password' placeholder='INPUT PASSWORD' value={password} onChange={e => setPassword(e.target.value)} required />
 						<button onClick={e => submitHandler(e)}>login</button>
 						<div style={{ textAlign: 'center' }}>
 							Don't have an account? <NavLink to={REGISTER_ROUTE}>register</NavLink>
@@ -47,9 +70,9 @@ const Auth = observer(() => {
 					</>
 					:
 					<>
-						<input type='text' placeholder='Input username' value={username} onChange={e => setUsername(e.target.value)} required />
-						<input type='password' placeholder='Input password' value={password} onChange={e => setPassword(e.target.value)} required />
-						<input type='password' placeholder='Confirm password' required />
+						<input type='text' placeholder='INPUT USERNAME' value={username} onChange={e => setUsername(e.target.value)} required />
+						<input type='password' placeholder='INPUT PASSWORD' value={password} onChange={e => setPassword(e.target.value)} required />
+						<input type='password' placeholder='CONFIRM PASSWORD' value={passwordConfirm} onChange={e => setPasswordConfirm(e.target.value)} required />
 						<button onClick={e => submitHandler(e)}>register</button>
 						<div style={{ textAlign: 'center' }}>
 							Have an account? <NavLink to={LOGIN_ROUTE}>login</NavLink>
--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,10 +1,10 @@
-export const MAIN_ROUTE = '/main'
-export const LOGIN_ROUTE = '/login'
-export const REGISTER_ROUTE = '/register'
-export const DEPOSIT_ROUTE = '/deposit'
-export const HI_LOW_ROUTE = '/hilow'
-export const DICE_ROUTE = '/dice'
-export const MINER_ROUTE = '/miner'
+export const MAIN_ROUTE = '/casinaki-project/main'
+export const LOGIN_ROUTE = '/casinaki-project/login'
+export const REGISTER_ROUTE = '/casinaki-project/register'
+export const DEPOSIT_ROUTE = '/casinaki-project/deposit'
+export const HI_LOW_ROUTE = '/casinaki-project/hilow'
+export const DICE_ROUTE = '/casinaki-project/dice'
+export const MINER_ROUTE = '/casinaki-project/miner'
 
 export const MIN_BET = 0.1
 export const MAX_BET = 999999.99
