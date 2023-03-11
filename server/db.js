d23b3fb7b8a01cdbde9dca929688ad1d03ca8eca
ladzislaf <ladzislaf@gmail.com>
2023-03-11 21:23:51 +0300

    change db to mysql

--- server/db.js
+++ server/db.js
@@ -5,8 +5,8 @@ module.exports = new Sequelize(
 	process.env.DB_USER,
 	process.env.DB_PASSWORD,
 	{
-		dialect: 'postgres',
 		host: process.env.DB_HOST,
-		port: process.env.DB_PORT
+		port: process.env.DB_PORT,
+		dialect: 'mysql' /* 'postgres' */
 	}
 )
