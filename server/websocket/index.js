83227694e01cb0680b7d89e8bff987d013ba1e90
ladzislaf <ladzislaf@gmail.com>
2023-05-13 10:28:56 +0300

    add websocket server

---
+++ server/websocket/index.js
@@ -0,0 +1,29 @@
+require('dotenv').config()
+const { Server } = require('socket.io')
+
+const io = new Server(5001, {
+	cors: {
+		origin: [process.env.CLIENT_URL],
+	},
+})
+
+let messages = []
+
+io.on('connection', (socket) => {
+	console.log(socket.id)
+	
+	socket.on('sync', () => {
+		socket.emit('sync', messages)
+	})
+
+	socket.on('sendMessage', (info) => {
+		console.log(info)
+		socket.broadcast.emit('recieveMessage', info.username, info.message)
+		messages.push(info)
+		if (messages.length >= 11) {
+			messages.shift()
+		}
+	})
+})
+
+
